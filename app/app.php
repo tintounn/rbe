e7ed6b760dc9969a4e43a58e6343d6b1a691204a
Quentin <pismont.quentin@orange.fr>
2018-02-20 16:04:10 +0100

    First commit

---
+++ app/app.php
@@ -0,0 +1,9 @@
+require '../vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->getBody()->write("Hello, " . $args['name']);
+});
+
+$app->run();
